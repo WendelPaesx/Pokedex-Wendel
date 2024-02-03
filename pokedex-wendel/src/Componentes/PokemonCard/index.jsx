0814efdc4a51198784fcd9a606fabc5e227f454c
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2024-02-03 14:45:24 -0300

    .

--- pokedex-wendel/src/Componentes/PokemonCard/index.jsx
+++ pokedex-wendel/src/Componentes/PokemonCard/index.jsx
@@ -1,40 +1,47 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
+export default function PokemonCard({ name, pokemonId }) {
+  const formattedPokemonId = String(pokemonId).padStart(3, "0");
 
-
-
-export default function PokemonCard({name, pokemonId}) {
-  const formattedPokemonId = String(pokemonId).padStart(3, '0');
-  
-  const imagemPokemon =`img/png/${formattedPokemonId}.png`;
+  const imagemPokemon = `img/png/${formattedPokemonId}.png`;
   const capitalizedFirstName = name.charAt(0).toUpperCase() + name.slice(1);
-  
+
   return (
-    
-    <Card sx={{ maxWidth: 380, border:'2px solid #ccc', borderRadius: '8px',  backgroundColor: '#E6E6FA' }}>
+    <Card
+      sx={{
+        maxWidth: 380,
+        border: "2px solid #ccc",
+        borderRadius: "8px",
+        backgroundImage: "linear-gradient(to bottom, #fff 50%, #ff0000)",
+      }}
+    >
       <CardMedia
-       
         component="img"
         alt={` ${formattedPokemonId}`}
         height="240"
-        image = {imagemPokemon}
-        
+        image={imagemPokemon}
       />
-      <CardContent sx={{display: 'flex', flexDirection:'column', alignItems: 'center', justifyContent:'center'}}>
+      <CardContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Typography gutterBottom variant="h5" component="div">
           <h2>{capitalizedFirstName}</h2>
         </Typography>
-        
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small"> Share</Button>
+        <Button size="small"> Curtir</Button>
       </CardActions>
     </Card>
   );
--- pokedex-wendel/src/services/index.jsx
+++ pokedex-wendel/src/services/index.jsx
@@ -18,6 +18,7 @@ const usePokemonService = () => {
   }, []);
 
   return { pokemons };
+  
 };
 
 export default usePokemonService;
